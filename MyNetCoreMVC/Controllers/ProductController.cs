7a70ab414db523e86405c42e0bdda18753bf01b7
hanhhg0607 <hanhgtd00593@fpt.edu.vn>
2018-10-27 13:42:28 +0700

    nop bai

---
+++ MyNetCoreMVC/Controllers/ProductController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyNetCoreMVC.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult GetList()
+        {
+            return View();
+        }
+        public IActionResult Create(string name , string price)
+        {
+            ViewData["name"] = name;
+            ViewData["price"] = price;
+
+            return View();
+        }
+        public IActionResult Update()
+        {
+            return View();
+        }
+        public IActionResult Delete(int id)
+        {
+            return new JsonResult(new Models.Product()
+            {
+                id = id
+            });
+        }
+    }
+}
